2a1f70beb7d30dd2d859957ae7e38b36b43618ee
Julie Agopian <ext.malt-community-sa.jagopian@systra.com>
2025-05-06 17:31:53 +0200

    Add render url to allowed_host

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "score-board-back.onrender.com",
+]
